fa08e1f11ff3b7d59c02a5576c1033c5f3e784c6
Karan Thakkar <karanjthakkar@gmail.com>
2017-02-08 10:43:58 +0530

    Add open source section

---
+++ index.js
@@ -0,0 +1,221 @@
+var oss = [{
+  "url": "https://github.com/tjunnone/npm-check-updates",
+  "name": "tjunnone/npm-check-updates",
+  "stars": 2535,
+  "forks": 90,
+  "prs": [{
+    "url": "https://github.com/tjunnone/npm-check-updates/issues/321",
+    "title": "Fix --silent option regression",
+    "state": "open"
+  }, {
+    "url": "https://github.com/tjunnone/npm-check-updates/issues/314",
+    "title": "Remove version ranges from final version based on flag",
+    "state": "closed"
+  }, {
+    "url": "https://github.com/tjunnone/npm-check-updates/issues/311",
+    "title": "Optionally include prereleases",
+    "state": "open"
+  }]
+}, {
+  "url": "https://github.com/freeCodeCamp/freeCodeCamp",
+  "name": "freeCodeCamp/freeCodeCamp",
+  "stars": 229279,
+  "forks": 9290,
+  "prs": [{
+    "url": "https://github.com/freeCodeCamp/freeCodeCamp/issues/10249",
+    "title": "Update contribution guideline",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/airbnb/javascript",
+  "name": "airbnb/javascript",
+  "stars": 46893,
+  "forks": 8997,
+  "prs": [{
+    "url": "https://github.com/airbnb/javascript/issues/1110",
+    "title": "Add section on multi-line imports. Fixes #1051",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/google/material-design-lite",
+  "name": "google/material-design-lite",
+  "stars": 25828,
+  "forks": 4398,
+  "prs": [{
+    "url": "https://github.com/google/material-design-lite/issues/4373",
+    "title": "Possible fix for #1398",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/yarnpkg/yarn",
+  "name": "yarnpkg/yarn",
+  "stars": 22258,
+  "forks": 88,
+  "prs": [{
+    "url": "https://github.com/yarnpkg/yarn/issues/1243",
+    "title": "Add an option to not commit to git. Fixes #1228",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/facebook/draft-js",
+  "name": "facebook/draft-js",
+  "stars": 8948,
+  "forks": 753,
+  "prs": [{
+    "url": "https://github.com/facebook/draft-js/issues/719",
+    "title": "Attempt to fix same charcter selection and replacing bug.",
+    "state": "open"
+  }]
+}, {
+  "url": "https://github.com/GoogleChrome/lighthouse",
+  "name": "GoogleChrome/lighthouse",
+  "stars": 6948,
+  "forks": 263,
+  "prs": [{
+    "url": "https://github.com/GoogleChrome/lighthouse/issues/912",
+    "title": "Add audit impl for rel=noopener for external links",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/vvo/gifify",
+  "name": "vvo/gifify",
+  "stars": 3338,
+  "forks": 150,
+  "prs": [{
+    "url": "https://github.com/vvo/gifify/issues/68",
+    "title": "Add option for no-loop. Fixes #66",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/feross/webtorrent-desktop",
+  "name": "feross/webtorrent-desktop",
+  "stars": 2957,
+  "forks": 274,
+  "prs": [{
+    "url": "https://github.com/feross/webtorrent-desktop/issues/1043",
+    "title": "Add a new Transfers menu to allow pause all and resume all torrents",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/redfin/react-server",
+  "name": "redfin/react-server",
+  "stars": 2941,
+  "forks": 116,
+  "prs": [{
+    "url": "https://github.com/redfin/react-server/issues/719",
+    "title": "Use add-module-exports to reduce .default in require.",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/emberjs/data",
+  "name": "emberjs/data",
+  "stars": 2750,
+  "forks": 1107,
+  "prs": [{
+    "url": "https://github.com/emberjs/data/issues/3646",
+    "title": "Replace instances of Object.create(null) with new EmptyObject().",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/sindresorhus/query-string",
+  "name": "sindresorhus/query-string",
+  "stars": 766,
+  "forks": 95,
+  "prs": [{
+    "url": "https://github.com/sindresorhus/query-string/issues/63",
+    "title": "Do not sort array values while stringify. Fix tests.",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/react-ga/react-ga",
+  "name": "react-ga/react-ga",
+  "stars": 624,
+  "forks": 74,
+  "prs": [{
+    "url": "https://github.com/react-ga/react-ga/issues/73",
+    "title": "Add a send() method for directly accessing ga.send().",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/ubilabs/react-geosuggest",
+  "name": "ubilabs/react-geosuggest",
+  "stars": 403,
+  "forks": 165,
+  "prs": [{
+    "url": "https://github.com/ubilabs/react-geosuggest/issues/262",
+    "title": "Allow adding custom classes to the suggest list and suggest item",
+    "state": "closed"
+  }, {
+    "url": "https://github.com/ubilabs/react-geosuggest/issues/252",
+    "title": "Do not use setState to save timer instance.",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/npm/marky-markdown",
+  "name": "npm/marky-markdown",
+  "stars": 265,
+  "forks": 44,
+  "prs": [{
+    "url": "https://github.com/npm/marky-markdown/issues/271",
+    "title": "Remove dist",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/GoogleChrome/web-push-encryption",
+  "name": "GoogleChrome/web-push-encryption",
+  "stars": 78,
+  "forks": 22,
+  "prs": [{
+    "url": "https://github.com/GoogleChrome/web-push-encryption/issues/49",
+    "title": "Update package.json to include repo name and other meta info. Fixes #30",
+    "state": "closed"
+  }]
+}, {
+  "url": "https://github.com/mozilla/science.mozilla.org",
+  "name": "mozilla/science.mozilla.org",
+  "stars": 20,
+  "forks": 22,
+  "prs": [{
+    "url": "https://github.com/mozilla/science.mozilla.org/issues/484",
+    "title": "Update punctuations and verbiage",
+    "state": "closed"
+  }]
+}]
+
+
+var html = oss.map(function(repo) {
+  return (
+    '<li class="repos__item">' +
+        '<div class="repos__item__header">' +
+          '<a class="repos__item__name" href="' + repo.url + '">' +
+            repo.name +
+          '</a>' +
+          '<div>' +
+            '<span class="repos__item__stat">' +
+              '<i class="icon-star"></i>' +
+              repo.stars +
+            '</span>' +
+            '<span class="repos__item__stat">' +
+              '<i class="icon-fork"></i>' +
+              repo.forks +
+            '</span>' +
+          '</div>' +
+        '</div>' +
+        '<span class="repos__item__body">Pull Requests:</span>' +
+        '<ul class="repos__item__list">' +
+          repo.prs.map(function(pr) {
+            return (
+              '<li class="repos__item__list__item">' +
+                '- <a href="' + pr.url + '">' +
+                  pr.title +
+                '</a>' +
+                '&nbsp;[' + pr.state + ']' +
+              '</li>'
+            );
+          }).join('') +
+        '</ul>' +
+    '</li>'
+  );
+});
+
+$('.js-repos-list').html(html.join(''));
