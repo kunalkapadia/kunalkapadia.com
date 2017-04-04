01151fb5006ef807ad9e4a1f1dce19140c55c31c
Karan Thakkar <karanjthakkar@gmail.com>
2017-04-04 23:47:06 +0530

    Update OSS contributions

--- index.js
+++ index.js
@@ -1,186 +1,337 @@
-var oss = [{
-  "url": "https://github.com/tjunnone/npm-check-updates",
-  "name": "tjunnone/npm-check-updates",
-  "stars": 2535,
-  "forks": 90,
-  "prs": [{
-    "url": "https://github.com/tjunnone/npm-check-updates/issues/321",
-    "title": "Fix --silent option regression",
-    "state": "open"
-  }, {
-    "url": "https://github.com/tjunnone/npm-check-updates/issues/314",
-    "title": "Remove version ranges from final version based on flag",
-    "state": "closed"
-  }, {
-    "url": "https://github.com/tjunnone/npm-check-updates/issues/311",
-    "title": "Optionally include prereleases",
-    "state": "open"
-  }]
-}, {
-  "url": "https://github.com/freeCodeCamp/freeCodeCamp",
-  "name": "freeCodeCamp/freeCodeCamp",
-  "stars": 229279,
-  "forks": 9290,
-  "prs": [{
-    "url": "https://github.com/freeCodeCamp/freeCodeCamp/issues/10249",
-    "title": "Update contribution guideline",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/airbnb/javascript",
-  "name": "airbnb/javascript",
-  "stars": 46893,
-  "forks": 8997,
-  "prs": [{
-    "url": "https://github.com/airbnb/javascript/issues/1110",
-    "title": "Add section on multi-line imports. Fixes #1051",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/google/material-design-lite",
-  "name": "google/material-design-lite",
-  "stars": 25828,
-  "forks": 4398,
-  "prs": [{
-    "url": "https://github.com/google/material-design-lite/issues/4373",
-    "title": "Possible fix for #1398",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/yarnpkg/yarn",
-  "name": "yarnpkg/yarn",
-  "stars": 22258,
-  "forks": 88,
-  "prs": [{
-    "url": "https://github.com/yarnpkg/yarn/issues/1243",
-    "title": "Add an option to not commit to git. Fixes #1228",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/facebook/draft-js",
-  "name": "facebook/draft-js",
-  "stars": 8948,
-  "forks": 753,
-  "prs": [{
-    "url": "https://github.com/facebook/draft-js/issues/719",
-    "title": "Attempt to fix same charcter selection and replacing bug.",
-    "state": "open"
-  }]
-}, {
-  "url": "https://github.com/GoogleChrome/lighthouse",
-  "name": "GoogleChrome/lighthouse",
-  "stars": 6948,
-  "forks": 263,
-  "prs": [{
-    "url": "https://github.com/GoogleChrome/lighthouse/issues/912",
-    "title": "Add audit impl for rel=noopener for external links",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/vvo/gifify",
-  "name": "vvo/gifify",
-  "stars": 3338,
-  "forks": 150,
-  "prs": [{
-    "url": "https://github.com/vvo/gifify/issues/68",
-    "title": "Add option for no-loop. Fixes #66",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/feross/webtorrent-desktop",
-  "name": "feross/webtorrent-desktop",
-  "stars": 2957,
-  "forks": 274,
-  "prs": [{
-    "url": "https://github.com/feross/webtorrent-desktop/issues/1043",
-    "title": "Add a new Transfers menu to allow pause all and resume all torrents",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/redfin/react-server",
-  "name": "redfin/react-server",
-  "stars": 2941,
-  "forks": 116,
-  "prs": [{
-    "url": "https://github.com/redfin/react-server/issues/719",
-    "title": "Use add-module-exports to reduce .default in require.",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/emberjs/data",
-  "name": "emberjs/data",
-  "stars": 2750,
-  "forks": 1107,
-  "prs": [{
-    "url": "https://github.com/emberjs/data/issues/3646",
-    "title": "Replace instances of Object.create(null) with new EmptyObject().",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/sindresorhus/query-string",
-  "name": "sindresorhus/query-string",
-  "stars": 766,
-  "forks": 95,
-  "prs": [{
-    "url": "https://github.com/sindresorhus/query-string/issues/63",
-    "title": "Do not sort array values while stringify. Fix tests.",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/react-ga/react-ga",
-  "name": "react-ga/react-ga",
-  "stars": 624,
-  "forks": 74,
-  "prs": [{
-    "url": "https://github.com/react-ga/react-ga/issues/73",
-    "title": "Add a send() method for directly accessing ga.send().",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/ubilabs/react-geosuggest",
-  "name": "ubilabs/react-geosuggest",
-  "stars": 403,
-  "forks": 165,
-  "prs": [{
-    "url": "https://github.com/ubilabs/react-geosuggest/issues/262",
-    "title": "Allow adding custom classes to the suggest list and suggest item",
-    "state": "closed"
-  }, {
-    "url": "https://github.com/ubilabs/react-geosuggest/issues/252",
-    "title": "Do not use setState to save timer instance.",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/npm/marky-markdown",
-  "name": "npm/marky-markdown",
-  "stars": 265,
-  "forks": 44,
-  "prs": [{
-    "url": "https://github.com/npm/marky-markdown/issues/271",
-    "title": "Remove dist",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/GoogleChrome/web-push-encryption",
-  "name": "GoogleChrome/web-push-encryption",
-  "stars": 78,
-  "forks": 22,
-  "prs": [{
-    "url": "https://github.com/GoogleChrome/web-push-encryption/issues/49",
-    "title": "Update package.json to include repo name and other meta info. Fixes #30",
-    "state": "closed"
-  }]
-}, {
-  "url": "https://github.com/mozilla/science.mozilla.org",
-  "name": "mozilla/science.mozilla.org",
-  "stars": 20,
-  "forks": 22,
-  "prs": [{
-    "url": "https://github.com/mozilla/science.mozilla.org/issues/484",
-    "title": "Update punctuations and verbiage",
-    "state": "closed"
-  }]
-}]
+var oss = [
+  {
+    url: 'https://github.com/facebook/react-native',
+    name: 'facebook/react-native',
+    stars: 45495,
+    forks: 10474,
+    prs: [
+      {
+        url: 'https://github.com/facebook/react-native/pull/13273',
+        title: 'Allow configuring the app source folder for android',
+        state: 'open'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/tjunnone/npm-check-updates',
+    name: 'tjunnone/npm-check-updates',
+    stars: 2638,
+    forks: 92,
+    prs: [
+      {
+        url: 'https://github.com/tjunnone/npm-check-updates/pull/336',
+        title: 'Remove global options usage',
+        state: 'merged'
+      },
+      {
+        url: 'https://github.com/tjunnone/npm-check-updates/pull/321',
+        title: 'Fix --silent option regression',
+        state: 'merged'
+      },
+      {
+        url: 'https://github.com/tjunnone/npm-check-updates/pull/314',
+        title: 'Remove version ranges from final version based on flag',
+        state: 'merged'
+      },
+      {
+        url: 'https://github.com/tjunnone/npm-check-updates/pull/311',
+        title: 'Optionally include prereleases',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/react-community/react-navigation',
+    name: 'react-community/react-navigation',
+    stars: 3034,
+    forks: 255,
+    prs: [
+      {
+        url: 'https://github.com/react-community/react-navigation/pull/734',
+        title: 'fix(docs): Fix incorrect style in TabNavigator sample',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/librariesio/libraries.io',
+    name: 'librariesio/libraries.io',
+    stars: 369,
+    forks: 47,
+    prs: [
+      {
+        url: 'https://github.com/librariesio/libraries.io/pull/1277',
+        title: 'Fix API doc links to be less github specific',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/nodejs/node',
+    name: 'nodejs/node',
+    stars: 32661,
+    forks: 6148,
+    prs: [
+      {
+        url: 'https://github.com/nodejs/node/pull/11504',
+        title: 'docs: Fix broken fg(1) links',
+        state: 'closed'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/webpack/webpack-cli',
+    name: 'webpack/webpack-cli',
+    stars: 228,
+    forks: 15,
+    prs: [
+      {
+        url: 'https://github.com/webpack/webpack-cli/pull/62',
+        title: '(docs): Make the contributing guide grammar better',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/ubilabs/react-geosuggest',
+    name: 'ubilabs/react-geosuggest',
+    stars: 442,
+    forks: 181,
+    prs: [
+      {
+        url: 'https://github.com/ubilabs/react-geosuggest/pull/262',
+        title: 'Allow adding custom classes to the suggest list and suggest item',
+        state: 'merged'
+      },
+      {
+        url: 'https://github.com/ubilabs/react-geosuggest/pull/252',
+        title: 'Do not use setState to save timer instance.',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/GoogleChrome/lighthouse',
+    name: 'GoogleChrome/lighthouse',
+    stars: 7736,
+    forks: 311,
+    prs: [
+      {
+        url: 'https://github.com/GoogleChrome/lighthouse/pull/912',
+        title: 'Add audit impl for rel=noopener for external links',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/vvo/gifify',
+    name: 'vvo/gifify',
+    stars: 3413,
+    forks: 157,
+    prs: [
+      {
+        url: 'https://github.com/vvo/gifify/pull/68',
+        title: 'Add option for no-loop. Fixes #66',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/npm/marky-markdown',
+    name: 'npm/marky-markdown',
+    stars: 267,
+    forks: 43,
+    prs: [
+      {
+        url: 'https://github.com/npm/marky-markdown/pull/271',
+        title: 'Remove dist',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/yarnpkg/yarn',
+    name: 'yarnpkg/yarn',
+    stars: 23369,
+    forks: 972,
+    prs: [
+      {
+        url: 'https://github.com/yarnpkg/yarn/pull/1243',
+        title: 'Add an option to not commit to git. Fixes #1228',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/redfin/react-server',
+    name: 'redfin/react-server',
+    stars: 2986,
+    forks: 117,
+    prs: [
+      {
+        url: 'https://github.com/redfin/react-server/pull/719',
+        title: 'Use add-module-exports to reduce .default in require.',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/facebook/draft-js',
+    name: 'facebook/draft-js',
+    stars: 9300,
+    forks: 811,
+    prs: [
+      {
+        url: 'https://github.com/facebook/draft-js/pull/719',
+        title: 'Attempt to fix same charcter selection and replacing bug.',
+        state: 'open'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/feross/webtorrent-desktop',
+    name: 'feross/webtorrent-desktop',
+    stars: 3161,
+    forks: 299,
+    prs: [
+      {
+        url: 'https://github.com/feross/webtorrent-desktop/pull/1043',
+        title: 'Add a new Transfers menu to allow pause all and resume all torrents',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/airbnb/javascript',
+    name: 'airbnb/javascript',
+    stars: 48856,
+    forks: 9367,
+    prs: [
+      {
+        url: 'https://github.com/airbnb/javascript/pull/1110',
+        title: 'Add section on multi-line imports. Fixes #1051',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/freeCodeCamp/freeCodeCamp',
+    name: 'freeCodeCamp/freeCodeCamp',
+    stars: 243773,
+    forks: 10064,
+    prs: [
+      {
+        url: 'https://github.com/freeCodeCamp/freeCodeCamp/pull/10249',
+        title: 'Update contribution guideline',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/sindresorhus/query-string',
+    name: 'sindresorhus/query-string',
+    stars: 837,
+    forks: 102,
+    prs: [
+      {
+        url: 'https://github.com/sindresorhus/query-string/pull/63',
+        title: 'Do not sort array values while stringify. Fix tests.',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/mozilla/science.mozilla.org',
+    name: 'mozilla/science.mozilla.org',
+    stars: 27,
+    forks: 35,
+    prs: [
+      {
+        url: 'https://github.com/mozilla/science.mozilla.org/pull/484',
+        title: 'Update punctuations and verbiage',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/react-ga/react-ga',
+    name: 'react-ga/react-ga',
+    stars: 708,
+    forks: 79,
+    prs: [
+      {
+        url: 'https://github.com/react-ga/react-ga/pull/73',
+        title: 'Add a send() method for directly accessing ga.send().',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/google/material-design-lite',
+    name: 'google/material-design-lite',
+    stars: 26388,
+    forks: 4503,
+    prs: [
+      {
+        url: 'https://github.com/google/material-design-lite/pull/4373',
+        title: 'Possible fix for #1398',
+        state: 'closed'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/GoogleChrome/web-push-encryption',
+    name: 'GoogleChrome/web-push-encryption',
+    stars: 78,
+    forks: 22,
+    prs: [
+      {
+        url: 'https://github.com/GoogleChrome/web-push-encryption/pull/49',
+        title: 'Update package.json to include repo name and other meta info. Fixes #30',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/emberjs/data',
+    name: 'emberjs/data',
+    stars: 2757,
+    forks: 1113,
+    prs: [
+      {
+        url: 'https://github.com/emberjs/data/pull/3646',
+        title: 'Replace instances of Object.create(null) with new EmptyObject().',
+        state: 'closed'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/Skalar/ember-keyboard-shortcuts',
+    name: 'Skalar/ember-keyboard-shortcuts',
+    stars: 42,
+    forks: 16,
+    prs: [
+      {
+        url: 'https://github.com/Skalar/ember-keyboard-shortcuts/pull/8',
+        title: 'Removed usage of prototype extensions',
+        state: 'merged'
+      }
+    ]
+  },
+  {
+    url: 'https://github.com/cfjedimaster/brackets-caniuse',
+    name: 'cfjedimaster/brackets-caniuse',
+    stars: 31,
+    forks: 9,
+    prs: [
+      {
+        url: 'https://github.com/cfjedimaster/brackets-caniuse/pull/9',
+        title: 'Table style and height modifications',
+        state: 'merged'
+      }
+    ]
+  }
+];
 
 
 var html = oss.map(function(repo) {
